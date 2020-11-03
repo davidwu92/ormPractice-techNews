9067b493c851d6fb75bbbaab04e62bc265f9dc7e
David Wu <davidjwu92@gmail.com>
2020-11-02 20:29:35 -0800

    comment.js changes

--- models/Comment.js
+++ models/Comment.js
@@ -3,16 +3,40 @@ const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
-Comment.init(
-  {
-    // columns will go here
+const commentSchema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'comment'
+  comment_text: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  post_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'post',
+      key: 'id'
+    }
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
   }
-);
+}
+
+const tableConfig = {
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'comment'
+}
+
+Comment.init(commentSchema,tableConfig);
 
 module.exports = Comment;
